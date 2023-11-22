5dd7b84085260513f78767d792fd31555d999eae
alex <beletskii1995@mail.ru>
2023-11-23 00:30:38 +1000

    Homework3

---
+++ Homework3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+Console.Clear();
+Console.Write("Введите число от 10 до 99: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 10 || n > 99) 
+{
+    Console.Write("Вы ошиблись!\nВведите число от 10 до 99: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+if (n / 10 > n % 10)
+{
+     Console.WriteLine ($" {n / 10}");
+}
+else
+{
+    Console.WriteLine ($" {n % 10}");
+}
