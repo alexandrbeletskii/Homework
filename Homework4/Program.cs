83125409a7b9b000970a91999c4c4ba09f50660b
alex <beletskii1995@mail.ru>
2023-11-23 00:30:56 +1000

    Homework4

---
+++ Homework4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+Console.Clear();
+Console.Write("Введите натуральное число: ");
+int n= int.Parse(Console.ReadLine()!);
+int result = 0;
+int y = n.ToString().Length;
+ 
+while (n > 0)
+ {
+ result *= 10;
+ result += n % 10;
+ n /= 10;
+}
+ 
+ for (int i = 0; i < y; i++)
+{
+ Console.Write($"{result%10},");
+result /= 10;
+}
