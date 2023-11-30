9e4abb9a02dee4121fa52fd433a898732b78b97f
alex <beletskii1995@mail.ru>
2023-11-30 17:16:44 +1000

    Homework3.3

---
+++ Homework3.3/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+double [] FillArray(int size)
+{
+    double[] array = new double [size];
+    Random random = new Random ();
+       for (int i = 0; i < array.Length; i++)
+    {
+        double randomNumber = Math.Round (random.NextDouble () *10, 2);
+        array[i] = randomNumber;
+
+    }
+    return array;
+}
+
+void PrintArray(double[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Console.Write(inArray[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+double DiffMinMaxElement (double [] array)
+{
+        double min = array [0];
+        double max = array [0];
+        for (int i = 0; i < array.Length; i++)
+        {
+        if(array [i] > max) max = array [i]; 
+        if(array [i] < min) min = array[i];
+        }
+    double diff= max-min;
+    diff= Math.Round (diff, 2);
+    return diff;
+}
+
+int lengthArray = 10;
+double[] arr = FillArray(lengthArray);
+PrintArray(arr);
+double diffMinMaxElement = DiffMinMaxElement (arr);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива = {diffMinMaxElement}");
