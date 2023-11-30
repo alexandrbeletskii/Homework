80967fa154864e6da43ba324b483d5425434d8d3
alex <beletskii1995@mail.ru>
2023-11-30 17:16:29 +1000

    Homework3.2

---
+++ Homework3.2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 2: Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Console.Write(inArray[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int QuantityEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int lengthArray = 10;
+int[] arr = FillArray(lengthArray);
+PrintArray(arr);
+int quantityEvenNumbers = QuantityEvenNumbers(arr);
+Console.WriteLine($"Количество четных элементов массива  = {quantityEvenNumbers}");
