5c37a224022b564322e9cfbb69d34c501bc54af8
«Alex» <beletskii1995@mail.ru>
2023-12-04 22:13:25 +1000

    Homework4.2

---
+++ Homework4.1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+int SumDigit(int num) 
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10; 
+        num /= 10; 
+    }
+    return sum;
+}
+
+while (true)
+{
+       Console.WriteLine("Введите целое число. Для завершения введите 'q' или или число сумма цифр которого четная :");
+    string input = Console.ReadLine()!;
+
+        if (input.ToLower() == "q")
+    {
+        Console.WriteLine("Вы ввели 'q', программа завершена");
+        break;
+    }
+
+       if (int.TryParse(input, out int number))
+    {
+                int digitSum = SumDigit(number);
+
+        // Проверяем, является ли сумма цифр четной.
+        if (digitSum % 2 == 0)
+        {
+            Console.WriteLine($"Сумма цифр числа {number} чётная. Программа завершается.");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Продолжайте ввод.");
+        }
+    }
+    else
+    {
+                Console.WriteLine("Некорректный ввод.");
+    }
+}
---
+++ Homework4.2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void inputArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = new Random().Next(100, 1000); 
+}
+
+int CountEvenNumbers (int[] arr)
+{
+    int count = 0;
+    foreach (int element in arr)
+    {
+        if (element%2==0)
+        count++;
+    }
+    return count;
+}
+
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+inputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+int evenDigit = CountEvenNumbers(array);
+Console.WriteLine($"Количество четных чисел в массиве: {evenDigit}");
