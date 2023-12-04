3b924f525f93a37d6caa105658927e81d3dcec8e
«Alex» <beletskii1995@mail.ru>
2023-12-04 22:14:00 +1000

    Homework4.3

---
+++ Homework4.3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+void inputArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = new Random().Next(99, 1000); 
+}
+
+void ReverseArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length/2; i++)
+    {
+        int temp= arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+}
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+inputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReverseArray (array);
+Console.WriteLine($"Перевернутый массив: [{string.Join(", ", array)}]");
+
