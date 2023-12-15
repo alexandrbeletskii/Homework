4be8bf166a1ca5e4f0538694aaf1aae8b9346213
«Alex» <beletskii1995@mail.ru>
2023-12-16 00:07:29 +1000

    Homework6.3

---
+++ Homework6.3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+
+bool IsPalindrome(string a)
+{
+string normalized = new
+string(a.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+return normalized.SequenceEqual(normalized.Reverse());
+}
+
+
+
+
+
+string input = "Hello World , dlroW olleH";
+bool isPalindrome = IsPalindrome(input);
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
+
