c28009bea995ba40f3ca7bb782fa8ac58fcd3f9a
«Alex» <beletskii1995@mail.ru>
2023-12-16 00:07:08 +1000

    Homework6.1

---
+++ Homework6.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+Console.Clear();
+char[,] chars = new char[,] { { 'f', 'g' }, { 'k', 'd' } };
+string result = string.Empty;
+for (int i = 0; i < chars.GetLength(0); i++)
+{
+for (int j = 0; j < chars.GetLength(1); j++)
+{
+result += chars[i, j];
+}
+}
+Console.WriteLine($"{result}");
+
+
