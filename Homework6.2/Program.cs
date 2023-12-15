b6cac862ebc3c34576dc24498c18776c4496518b
«Alex» <beletskii1995@mail.ru>
2023-12-16 00:07:16 +1000

    Homework6.2

---
+++ Homework6.2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+Console.Clear();
+string input = "Hello World , Road to IT";
+string result = input.ToLower();
+Console.WriteLine($"{result}");
