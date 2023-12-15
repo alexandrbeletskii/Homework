ed6dbb904e4c6a57d0717f7434ddd27c26ff0c3a
«Alex» <beletskii1995@mail.ru>
2023-12-16 00:07:39 +1000

    Homework6.4

---
+++ Homework6.4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. Сформировать строку, в которой слова расположены в обратном порядке. В полученной строке слова должны быть также разделены пробелами.
+
+string input = "Hello my world";
+string result = ReverseWords(input);
+Console.WriteLine(result);
+
+// Метод для обращения порядка слов в строке
+ string ReverseWords(string str)
+{
+string[] words = str.Split(' ');
+Array.Reverse(words);
+return string.Join(" ", words);
+}
