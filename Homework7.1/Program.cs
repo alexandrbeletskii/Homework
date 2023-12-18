7cac5cff0b4415e596815f44fb962634f3e4a787
«Alex» <beletskii1995@mail.ru>
2023-12-19 00:58:31 +1000

    Homework7.1

---
+++ Homework7.1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+
+void printNumbers(int a, int b)
+{
+    if (a > b)
+        return;
+{
+    Console.Write($"{a} ");
+}
+printNumbers(a + 1, b);
+}
+
+
+
+Console.Clear();    
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите  число N: ");
+int n = int.Parse(Console.ReadLine()!);
+printNumbers(m,n);
+
+
+
