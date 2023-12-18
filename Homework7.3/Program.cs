8bac05f386e91fbf587c3532d0498073b9a6ebb5
«Alex» <beletskii1995@mail.ru>
2023-12-19 00:58:52 +1000

    Homework7.3

---
+++ Homework7.3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] FillArray(int size)
+{
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(1, 100);
+}
+return array;
+}
+
+void PrintArray(int[] inArray)
+{
+for (int i = 0; i < inArray.Length; i++)
+{
+Console.Write(inArray[i] + " ");
+}
+Console.WriteLine();
+}
+
+void ReverseArray(int[] array, int firstDigit=0)
+
+   {
+    if(firstDigit != array.Length) {
+        ReverseArray(array, firstDigit + 1);
+        Console.Write($"{array[firstDigit]} ");
+    }
+   }
+
+int lengthArray = 10;
+int[] arr = FillArray(lengthArray);
+Console.WriteLine($" Начальный массив: ");
+PrintArray (arr);
+ReverseArray (arr);
+
